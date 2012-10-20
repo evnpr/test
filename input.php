5a852558a6e99d84850f7f2afc0445c8c19d0b40
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:10:15 +0000

    kuntum just edit /workshop/input.php

--- input.php
+++ input.php
@@ -1,3 +1,25 @@
+<html>
+<head>
+<title>Workshop Kuntum</title>
+<style>
+#header{
+    height: 40px;
+    background-color: #eee;
+    margin-bottom: 50px;    
+}
+</style>
+
+</head>
+<body>
+<div id="header">
+    <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQs1cmHC867CVc0fLaSimKNGSagPNFd2lhERT5rqovMOQ4mKtw_" height="40" width="100" />
+    <span style="float: right; margin-right: 30px; margin-top: 10px;"><a href="register.php">Register</a></span>
+</div>
+
+<div id="container">
+<center>
+
+
 <form action="login.php" method="POST">
 Nama Produk : <input type="text" name="username" /><br>
 Jumlah Barang : <select name="jumlah">
@@ -15,3 +37,16 @@ for($i=0; $i<10; $i++){
 </select><br>
 <input type="submit" value="submit" />
 </form>
+</center>
+</div>
+
+
+<div id="footer">
+</div>
+
+
+</body>
+</html>
+
+
+
