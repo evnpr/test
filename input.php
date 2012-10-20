ec2b98133ed23a67297a8479f33de4c0aa9e6c14
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 07:35:32 +0000

    kuntum just edit /workshop/input.php

--- input.php
+++ input.php
@@ -1,5 +1,13 @@
 <form action="login.php" method="POST">
 Nama Produk : <input type="text" name="username" /><br>
-Jumlah Barang : <select name="jumlah"><br>
+Jumlah Barang : <select name="jumlah">
+<?php
+for($i=0; $i<10; $i++){
+?>
+<option value=""></option>
+</select><br>
+<?php
+}
+?>
 <input type="submit" value="submit" />
 </form>
