211c4686b2a7b594a6c0ea329bb0e5cde1f09059
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 07:24:31 +0000

    kuntum just edit /workshop/index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <title>Workshop Kuntum</title>
 </head>
 <body>
-
+<form action="login.php" method="POST">
+username : <input type="text" name="username" /><br>
+password : <input type="password" name="pwd" id="pwd"/><br>
+<input type="submit" value="login" />
+</form>
 </body>
 </html>
