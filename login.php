def18346947000590131d86ac00f5d122ffe1f5b
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 07:26:59 +0000

    kuntum just edit /workshop/login.php

--- login.php
+++ login.php
@@ -5,6 +5,8 @@
 
     $sql = "SELECT * FROM user WHERE username = $usernamepost
             AND pwd = $password";
+            
+            
     $result = mysql_query($sql);
     while($row = mysql_fetch_array($result)){
         $_SESSION['username'] = $usernamepost;
