5dd6caaf63c6b0c97e110131d4d90da1eb2ed0d9
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 07:40:08 +0000

    kuntum just edit /workshop/submit_input.php

--- submit_input.php
+++ submit_input.php
@@ -3,15 +3,7 @@
     $pwd = md5($_POST['pwd']);
     $email = $_POST['email'];
     
-    $result = mysql_query("SELECT * FROM user WHERE username = 'evan'");
-    while($row = mysql_fetch_array($result)){
-        $_SESSION['alert'] = 'exist';
-        header("Location: successpage.php");
-        return;
-    }
     
     $sql = "INSERT INTO user(username,pwd,email) VALUES('$usernamepost','$pwd','$email')";
     mysql_query($sql);
-    echo $_SESSION['username'];
-    $_SESSION['alert'] = 'success';
     header("Location: successpage.php");
