2b110e82cfb4f6ca1a124d2461f51ab141544184
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:26:32 +0000

    kuntum just edit /workshop/submit_register.php

--- submit_register.php
+++ submit_register.php
@@ -7,7 +7,7 @@
     $result = mysql_query("SELECT * FROM user WHERE username = 'evan'");
     while($row = mysql_fetch_array($result)){
         $_SESSION['alert'] = 'exist';
-        header("Location: successpage.php");
+        header("Location: showe.php");
         return;
     }
     
@@ -17,3 +17,4 @@
     $_SESSION['alert'] = 'success';
     header("Location: show.php");
 ?>
+
