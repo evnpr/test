0fce8fa94b099153c163d5fb8f9c67add873aeed
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:48:21 +0000

    kuntum just edit /workshop/submit_input.php

--- submit_input.php
+++ submit_input.php
@@ -1,6 +1,6 @@
 <?php include "db.php"; ?>
 <?php
-    $name = $_POST['nama'];
+    $name = $_POST['name'];
     $description = $_POST['description'];
     $jumlah = $_POST['jumlah'];
     
@@ -8,5 +8,6 @@
     $sql = "INSERT INTO product(name,description,jumlah) VALUES('$name','$description','$jumlah')";
     mysql_query($sql);
     header("Location: show.php");
+
 
 
