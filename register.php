bc4c228508eab2f8630791ae24b2310872b44095
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:24:14 +0000

    kuntum just edit /workshop/register.php

--- register.php
+++ register.php
@@ -12,7 +12,9 @@
 </head>
 <body>
 <div id="header">
+     <a href="index.php">
     <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQs1cmHC867CVc0fLaSimKNGSagPNFd2lhERT5rqovMOQ4mKtw_" height="40" width="100" />
+    </a>
     <span style="float: right; margin-right: 30px; margin-top: 10px;"><a href="register.php">Register</a></span>
 </div>
 
@@ -44,5 +46,6 @@ function validate(){
 
 </body>
 </html>
+
 
 
