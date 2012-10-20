98a1531205913ad2273d791fe2a0e4239bc2f683
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:04:32 +0000

    kuntum just edit /workshop/register.php

--- register.php
+++ register.php
@@ -1,3 +1,23 @@
+<html>
+<head>
+<title>Workshop Kuntum</title>
+<style>
+#header{
+    height: 40px;
+    background-color: #eee;
+    margin-bottom: 50px;    
+}
+</style>
+
+</head>
+<body>
+<div id="header">
+    <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQs1cmHC867CVc0fLaSimKNGSagPNFd2lhERT5rqovMOQ4mKtw_" height="40" width="100" />
+    <span style="float: right; margin-right: 30px; margin-top: 10px;"><a href="register.php">Register</a></span>
+</div>
+
+<div id="container">
+<center>
 <form action="submit_register.php" method="POST">
 username : <input type="text" name="username" /><br>
 password : <input type="password" name="pwd" id="pwd"/><br>
@@ -5,3 +25,13 @@ conf password : <input type="password" name="cpwd" id="cpwd"/><br>
 email : <input type="text" name="email"/><br>
 <input type="submit" value="login" />
 </form>
+</center>
+</div>
+
+
+<div id="footer">
+</div>
+
+
+</body>
+</html>
