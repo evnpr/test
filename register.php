eeb2749b1710d4bd0ea51656b7e45d6471f7fd2b
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-20 08:05:13 +0000

    kuntum just edit /workshop/register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@
 </div>
 
 <div id="container">
-
+<center>
 <form action="submit_register.php" method="POST">
 username : <input type="text" name="username" /><br>
 password : <input type="password" name="pwd" id="pwd"/><br>
@@ -25,7 +25,7 @@ conf password : <input type="password" name="cpwd" id="cpwd"/><br>
 email : <input type="text" name="email"/><br>
 <input type="submit" value="login" />
 </form>
-
+</center>
 </div>
 
 
